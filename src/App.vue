33bd2077fc80c181696d4bc06eb335f51388bf73
Ahrorbek Uralbayev <ahrorbek.uralbayev1@gmail.com>
2023-07-23 21:11:04 +0500

    done

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <component :is="$route.meta.layout || 'div'">
+      <router-view />
+    </component>
+  </main>
+</template>
+
+
+<style>
+.container {
+  width: 100%;
+  max-width: 1224px;
+  padding: 0px 12px;
+  margin: auto;
+}
+</style>
